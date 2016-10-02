c287bf5bbeed2ac37cfe505c54ee76a7ff9a685e
Jochen Deferme <jochen.deferme@gmail.com>
2016-10-02 18:37:42 +0200

    general structure in place but need to be glued together

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -3,10 +3,30 @@
 
 angular.module('NarrowItDownApp', [])
 .controller('NarrowItDownController', NarrowItDownController)
-.service('MenuSearchService', MenuSearchService);
+.service('MenuSearchService', MenuSearchService)
+.directive('foundItems', FoundItemsDirective);
+
+function FoundItemsDirective() {
+  var ddo = {
+    templateUrl: 'foundItems.html',
+    scope: {
+      found: '<',
+      onRemove: '&'
+    },
+    controller: NarrowItDownController,
+    controllerAs: 'list',
+    bindToController: true
+  };
+  return ddo;
+}
 
 NarrowItDownController.$inject = ['MenuSearchService'];
 function NarrowItDownController(MenuSearchService) {
+  var list = this;
+  // list.searchTerm ="";
+  var promise = MenuSearchService.getMatchedMenuItems(list.searchTerm);
+
+  // list.items = menuItems.getMatchedMenuItems(list.searchTerm);
 
 }
 
@@ -20,14 +40,15 @@ function MenuSearchService($http) {
       method: "GET",
       url: ("https://davids-restaurant.herokuapp.com/menu_items.json")
     });
-    return response.then(function(result)) {
-      // process result and only keep items that match
-      var foundItems = [];
-
-
-      // return processed items
-      return foundItems;
-    }
+    return response;
+    // .then(function(result)) {
+    //   // process result and only keep items that match
+    //   var foundItems = result;
+    //   console.log('result');
+    //
+    //   // return processed items
+    //   return foundItems;
+    // }
 
   }
 
