f630063cd49fc3a8024cdc8c9312e86421ddd8cc
Jochen Deferme <jochen.deferme@gmail.com>
2016-10-02 19:28:24 +0200

    currently stuck

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -10,51 +10,59 @@ function FoundItemsDirective() {
   var ddo = {
     templateUrl: 'foundItems.html',
     scope: {
-      found: '<',
+      foundItems: '<',
       onRemove: '&'
     },
-    controller: NarrowItDownController,
-    controllerAs: 'list',
+    controller: NarrowItDownController, //FoundItemsDirectiveController
+    controllerAs: 'controller',
     bindToController: true
   };
   return ddo;
 }
 
+// function FoundItemsDirectiveController(){
+//  var controller = this;
+// }
+
 NarrowItDownController.$inject = ['MenuSearchService'];
 function NarrowItDownController(MenuSearchService) {
-  var list = this;
-  // list.searchTerm ="";
-  var promise = MenuSearchService.getMatchedMenuItems(list.searchTerm);
+  var controller = this;
+  controller.searchTerm ="";
+  controller.foundItems ="";
 
-  // list.items = menuItems.getMatchedMenuItems(list.searchTerm);
+  controller.getMatchedMenuItems = function(searchTerm) {
+    var promise = MenuSearchService.getMatchedMenuItems();
+    promise.then(function(response){
 
+      controller.foundItems = response.data.menu_items;
+      // console.log(controller.foundItems);
+    })
+  }
+  // .then(function(result)) {
+  //   // process result and only keep items that match
+  //   var foundItems = result;
+  //   console.log('result');
+  //
+  //   // return processed items
+  //   return foundItems;
+  // }
+  controller.removeItem = function(index){
+
+    //to be implemented still
+  }
 }
 
 MenuSearchService.$inject = ['$http']
 function MenuSearchService($http) {
   var service = this;
 
-  service.getMatchedMenuItems = function(searchTerm) {
-
+  service.getMatchedMenuItems = function() {
     var response = $http({
       method: "GET",
       url: ("https://davids-restaurant.herokuapp.com/menu_items.json")
-    });
+      });
     return response;
-    // .then(function(result)) {
-    //   // process result and only keep items that match
-    //   var foundItems = result;
-    //   console.log('result');
-    //
-    //   // return processed items
-    //   return foundItems;
-    // }
-
   }
-
-
 }
-
-
 
 })();
